3d06c5182b6db16dc960676ad9afe8b1864c52dc
Fifachaimong <fifasisa36@gmail.com>
2026-09-10 21:26:46 +0700

    feat: add port configuration to database connection pool

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -3,6 +3,7 @@ import mysql from 'mysql2/promise.js'
 const db = mysql.createPool({
     host : process.env.DATABASE_HOST,
     user : process.env.DATABASE_USER,
+    port : process.env.DATABASE_PORT,
     password : process.env.DATABASE_PASSWORD,
     database : process.env.DATABASE_NAME
 })
